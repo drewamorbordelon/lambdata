9ccc4772683c40ed32d969870dc22272f3054de1
drewamorbordelon <drew-bordelon@lambdastudents.com>
2020-10-27 14:09:23 -0500

    adding .py files from lecture - __init__.py, example.py, oop_example.py

---
+++ example.py
@@ -0,0 +1,16 @@
+"""Lambdata - a collection of Data Science helper functions"""
+
+#  accessing libraries through pipenv
+import pandas as pd
+import numpy as np
+
+Favorite_numbers = [77, 17, 27, 7, 3.14]
+
+def df_cleaner(df):
+    """Class a DF"""
+    #...TODO - implement df_cleaner
+    pass
+
+def df_destroyer(df):
+    """Destroys a DF"""
+    pass
---
+++ oop_example.py
@@ -0,0 +1,84 @@
+
+"""OOP examples for module 2"""
+
+import pandas as pd
+import numpy as np
+
+
+class MyDataFrame(pd.DataFrame):
+    def num_cells(self):
+        return self.shape[0] * self,shape[1]
+
+
+class BareClass:
+    pass
+
+
+class Complex:
+    def __init__(self, realpart, imagpart):
+        """
+        Constructor for complex numbers.
+        Complex numbers have a real part and imaginary part
+        """
+        self.r = realpart
+        self.i = imagpart
+
+    def add(self, other_complex):
+        self.r += other_complex.r
+        self.i += other_complex.i
+
+    def __repr__(self):
+        return "({},{})".format(self.r, self.i)
+
+
+class SocialMediaUser:
+    def __init__(self, name, location, upvotes=0):
+        self.nam = str(name)
+        self.loc = str(loc)
+        self.up += int(upvotes)
+
+    def receive_upvotes(self, num_up=1):
+        self.up += num_up
+
+    def is_popular(self):
+        return self.up > 100
+
+
+class Animal:
+    """General Representation of Animals"""
+    def __init__(self, name, weight, diet_type):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.diet_type = str(diet_type)
+
+    def run(self):
+        return "Vroom, Vroom, I go quick"
+
+    def eat(self, food):
+        return "Huge fan of the " + str(food)
+
+
+class Sloth(Animal):
+    def __init__(self, name, weight, diet_type, num_naps):
+        super().__init__(name, weight, diet_type)
+        self.num_naps = int(num_naps)
+
+    def say_something(self):
+        return "This is a sloth of typing"
+
+    def run(self):
+        return "Verrrrryyyy slooowwww"
+
+
+if __name__ == "__main__":
+    num1 = Complex(3, 5)
+    num2 = Complex(4, 2)
+    num1.add(num2)
+    print(num1.r, num1.i)
+
+    user1 = SocialMediaUser("Drew", "Nola", 101)
+    user2 = SocialMediaUser("Tori", "Biloxi", 1)
+    user3 = SocialMediaUser("D", "Seattle", upvotes=100)
+    user4 = SocialMediaUser("El", "NC", 2)
+    print("name: {}, is popular:{}".format(user2.nam, user2.is_popular, user2.up))
+    print("name: {}, is popular:{}".format(user1.nam, user2.is_popular, user1.up))
